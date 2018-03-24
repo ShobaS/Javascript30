0421806af930e45e20a485d0ea02258b62246a46
Shoba Selvaraj <shobaselvaraj.95@gmail.com>
2018-03-24 22:49:20 +0530

    js clock

---
+++ d2-clock/index.js
@@ -0,0 +1,19 @@
+(function(){
+  const secondHand = document.querySelector('.sec-hand');
+  const minHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+  function setDate(){
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hour = now.getHours();
+  const secondsDeg = ((seconds / 60) * 360) + 90;
+  const minsDeg = ((mins/60) * 360) +90;
+  const hourDeg = ((hour/12) * 360) +90;
+
+  secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+  minHand.style.transform = `rotate(${minsDeg}deg)`;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+}
+setInterval(setDate,1000);
+})();
